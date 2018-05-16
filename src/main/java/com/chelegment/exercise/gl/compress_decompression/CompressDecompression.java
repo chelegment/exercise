b766d1cfeb74eb9457b47f6d492a3beb11accf05
chelegment <chelegment@hotmail.com>
2018-05-16 22:39:44 +0300

    hello world

---
+++ src/main/java/com/chelegment/exercise/gl/compress_decompression/CompressDecompression.java
@@ -0,0 +1,8 @@
+package com.chelegment.exercise.gl.compress_decompression;
+
+public class CompressDecompression {
+
+    public static void main(String[] args) {
+        System.out.println("He110 W0r1D");
+    }
+}
